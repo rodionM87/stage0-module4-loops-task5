296bca727a9f612071aa06b9cca4177bb2d5a15e
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-04-11 21:09:40 +0600

    Update Triangle.java

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,17 +2,11 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
-        String strVar = "";
-        int count = 0;
-        if (cathetusLength == "") {
-            strVar = "test";
-        } else {        
-            for (char iter:cathetusLength.toCharArray()) {
-                if (iter == '\n') {
-                    count++;
-                }
+        for (char iter:cathetusLength.toCharArray()) {
+            if (iter == '\n') {
+                count++;
             }
-            System.out.println(count);
         }
+        System.out.println(count);
     }
 }
