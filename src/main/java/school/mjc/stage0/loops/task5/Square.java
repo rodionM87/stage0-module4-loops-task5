962a1afc6507a2282368c3a79bbb99ae71cc0b0a
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-04-11 21:08:35 +0600

    Update Square.java

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -2,18 +2,12 @@ package school.mjc.stage0.loops.task5;
 
 public class Square {
     public void printSquareFrom8s(int sideLength){
-        String strVar = "";
-        int count = 0;
-        if (sideLength == "") {
-            strVar = "test";
-        } else {        
-            for (char iter:sideLength.toCharArray()) {
-                if (iter == '\n') {
-                    count++;
-                }
+        for (char iter:sideLength.toCharArray()) {
+            if (iter == '\n') {
+                count++;
             }
-            System.out.println(count);
         }
+        System.out.println(count);
         
     }
 }
