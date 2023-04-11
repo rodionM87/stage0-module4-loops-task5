87b3f649b372a6218fb8d928808eb5a696a0faac
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-04-11 21:12:29 +0600

    Update Hourglass.java

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,7 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        int count = 0;
         for (char iter:height.toCharArray()) {
             if (iter == '\n') {
                 count++;
