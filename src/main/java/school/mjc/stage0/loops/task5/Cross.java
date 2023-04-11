de3c33411a0d6fad5959c5d2c0f8da29c85f3492
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-04-11 21:12:45 +0600

    Update Cross.java

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,7 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
+        int count = 0;
         for (char iter:sideLength.toCharArray()) {
             if (iter == '\n') {
                 count++;
