fcf1895e0e679914ed3ca30fbeaac1aec5ce95ee
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-04-26 11:00:27 +0600

    Update Cross.java

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,13 +2,29 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-        int count = 0;
-        for (char iter:sideLength.toCharArray()) {
-            if (iter == '\n') {
-                count++;
-            }
-        }
-        System.out.println(count);
-        
+        String side = "";
+		int middleVariable = sideLength / 2 + 1;
+		for (int iter = 1; iter <= sideLength; iter++) {
+			if (iter != middleVariable) {
+				side = " ";
+				for (int externalIter = 1; externalIter <= sideLength; externalIter++) {
+					if (externalIter == middleVariable) {
+						side += "8";
+					}
+					else if (externalIter != middleVariable) {
+						side += " ";
+					}
+				}								
+				System.out.println(side);
+			}
+			else if (iter == middleVariable) {
+				side = " ";
+				for (int intoMiddle = 1; intoMiddle <= sideLength; intoMiddle++) {
+					side += "8";
+				}
+				System.out.println(side);
+			}
+			
+		}
     }
 }
